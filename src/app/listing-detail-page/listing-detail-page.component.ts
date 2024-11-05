26792c467d5a5fe0752074973997e55c045fc063
whddnjs1715 <whddnjs1715@gmail.com>
2024-11-05 17:56:50 +0100

    initial commit

---
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-detail-page',
+  templateUrl: './listing-detail-page.component.html',
+  styleUrl: './listing-detail-page.component.css'
+})
+export class ListingDetailPageComponent {
+
+}
---
+++ src/app/listings-page/listings-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-page',
+  templateUrl: './listings-page.component.html',
+  styleUrl: './listings-page.component.css'
+})
+export class ListingsPageComponent {
+
+}
