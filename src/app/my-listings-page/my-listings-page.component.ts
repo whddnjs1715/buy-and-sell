e9caa2d377750e494b35f3c05d52db047997d0be
whddnjs1715 <whddnjs1715@gmail.com>
2024-11-06 16:25:04 +0100

    update mylistingspage

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
   styleUrl: './my-listings-page.component.css'
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit{
+  listings: Listing[] = [];
+  
+  constructor(){}
 
+  ngOnInit(): void {
+    console.log('fakeListings', fakeListings)
+    this.listings = fakeListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with id ${listingId}`)
+  }
 }
