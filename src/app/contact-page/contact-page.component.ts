cf315477443bfbe0227186144bcce0bb756cadf0
whddnjs1715 <whddnjs1715@gmail.com>
2024-11-06 15:35:53 +0100

    update detail page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { ActivatedRoute, Router } from '@angular/router';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit{
+  email: string = '';
+  message: string = '';
+  listing: Listing = {
+    id: '',
+    name: '',
+    description: '',
+    price: 0,
+  };
 
+  constructor (
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id) ?? {
+      id: '',
+      name: '',
+      description: '',
+      price: 0,
+    };
+    this.message = `Hi I'm interested in your ${this.listing.name.toLocaleLowerCase()}!`
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent!')
+    this.router.navigateByUrl('/listings');
+  }
 }
--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listing-detail-page',
   templateUrl: './listing-detail-page.component.html',
   styleUrl: './listing-detail-page.component.css'
 })
-export class ListingDetailPageComponent {
+export class ListingDetailPageComponent implements OnInit{
+  listing: Listing = {
+    id: '',
+    name: '',
+    description: '',
+    price: 0,
+  };
 
+  constructor (
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id) ?? {
+      id: '',
+      name: '',
+      description: '',
+      price: 0,
+    };
+  }
 }
