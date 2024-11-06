ea12adcaad778c8002a136dcee4a829a7dab7db5
whddnjs1715 <whddnjs1715@gmail.com>
2024-11-06 14:19:53 +0100

    update listings-page-component

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
   styleUrl: './listings-page.component.css'
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit{
+  listings: Listing[] = [];
 
+  constructor () {}
+
+  ngOnInit(): void {
+    this.listings = fakeListings    
+  }
 }
